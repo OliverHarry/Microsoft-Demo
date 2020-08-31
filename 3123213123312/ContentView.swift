f909069250751ff78e75e68ebaa23ff7b48d939e
man.li <man.li@shopee.com>
2020-08-31 14:26:10 +0800

    add soure files

---
+++ 3123213123312/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  3123213123312
+//
+//  Created by man on 8/31/20.
+//  Copyright © 2020 213213213. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 3123213123312Tests/_123213123312Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _123213123312Tests.swift
+//  3123213123312Tests
+//
+//  Created by man on 8/31/20.
+//  Copyright © 2020 213213213. All rights reserved.
+//
+
+import XCTest
+@testable import _123213123312
+
+class _123213123312Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
